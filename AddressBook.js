39cb0b5819b66fd4382945bbe6b213c890568e13
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-12-19 23:13:32 +0530

    Added: UC2 Ability to ensure valid contacts are added

--- AddressBook.js
+++ AddressBook.js
@@ -1,3 +1,19 @@
+let firstNameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+let lastNameRegex = RegExp("^[A-Z]{1}[A-Za-z]{3,}$");
+let addressRegex = RegExp("^.{4,}$");
+let cityRegex = RegExp("^[a-zA-Z]{4,}$");
+let stateRegex = RegExp("^[a-zA-Z\\s]{4,}$");
+let zipCodeRegex = RegExp("^[1-9]{1}[0-9]{2}[\\s]?[0-9]{3}$");
+let phoneRegex = RegExp("^[0-9]{10}$");
+let emailRegex = RegExp("^[a-zA-Z0-9+_-]+([.][a-zA-Z0-9]+)*@([a-zA-Z0-9]+)([.][a-z]+)?[.][a-z]{2,}$");
+
+class ValidationError extends Error {
+    constructor(message) {
+      super(message); 
+      this.name = "ValidationError"; 
+    }
+}
+
 class Contacts{
     //properties
     firstName;
@@ -11,14 +27,29 @@ class Contacts{
 
     //constructors
     constructor(...params){
-        this.firstName = params[0];
-        this.lastName = params[1];
-        this.address = params[2];
-        this.city = params[3];
-        this.state = params[4];
-        this.zipCode = params[5];
-        this.phone = params[6];
-        this.email = params[7];
+        if(firstNameRegex.test(params[0])) this.firstName = params[0];
+        else throw new ValidationError("Invalid First Name: "+params[0]);
+        
+        if(lastNameRegex.test(params[1])) this.lastName = params[1];
+        else throw new ValidationError("Invalid Last Name: "+params[1]);
+        
+        if(addressRegex.test(params[2])) this.address = params[2];
+        else throw new ValidationError("Invalid Address: "+params[2]);
+        
+        if(cityRegex.test(params[3])) this.city = params[3];
+        else throw new ValidationError("Invalid City: "+params[3]);
+        
+        if(stateRegex.test(params[4])) this.state = params[4];
+        else throw new ValidationError("Invalid State: "+params[4]);
+        
+        if(zipCodeRegex.test(params[5])) this.zipCode = params[5];
+        else throw new ValidationError("Invalid ZIP Code: "+params[5]);
+        
+        if(phoneRegex.test(params[6])) this.phone = params[6];
+        else throw new ValidationError("Invalid Phone Number: "+params[6]);
+        
+        if(emailRegex.test(params[7])) this.email = params[7];
+        else throw new ValidationError("Invalid Email: "+params[7]);
     }
 
     //toString method
@@ -31,5 +62,6 @@ class Contacts{
 
 //Create an obj
 let contact = new Contacts("Krutika","Patil","Deopur","Dhule","Maharashtra",424006,
-                                    "9876543210","kp@gmail.com");
+                                    "9897654321","kp@gmail.com");
 process.stdout.write(contact.toString());
+
